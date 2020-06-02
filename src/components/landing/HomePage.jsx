9ab7bf35cff55d834475178b38eb3ce327b8188b
jamshidi.m799 <jamshidi.m799@gmail.com>
2020-06-02 13:45:57 +0430

    create project

---
+++ src/components/landing/HomePage.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+// import HeadTop from './comp/HeadTop'
+// import Sponser from './comp/sponser';
+// import About from './comp/About'
+// import Scores from './comp/Scores';
+// import CommentShow from './comp/CommentShow';
+// import Award from './comp/Award';
+
+export default class HomePage extends Component {
+  render() {
+    return (
+      <section className="homepage">
+        <div></div>
+        {/* <HeadTop />
+        <Sponser />
+        <About />
+        <Scores />
+        <CommentShow />
+        <Award /> */}
+      </section>
+    );
+  }
+}
